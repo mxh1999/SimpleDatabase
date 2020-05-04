7bed8af3b962ca363e65bff7a54d6032af8c2154
mxh1999412 <mxh1999@126.com>
2020-05-04 13:00:36 +0800

    start lab3

---
+++ acmdb-lab3/src/java/simpledb/Tuple.java
@@ -0,0 +1,126 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private TupleDesc disc;
+    private RecordId id;
+    private Field[] flds;
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        disc = td;
+        id = null;
+        flds = new Field[td.numFields()];
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        return disc;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        return id;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        id = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        flds[i]=f;
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        return flds[i];
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        StringBuffer s = new StringBuffer();
+        for (int i =0;i<flds.length;i++) {
+            if (i>0) s.append("\t");
+            s.append(flds[i].toString());
+        }
+        return s.toString();
+    }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        return new Iterator<Field>() {
+            private int pos= 0;
+            @Override
+            public boolean hasNext() {
+                return pos < flds.length;
+            }
+
+            @Override
+            public Field next() {
+                return flds[pos++];
+            }
+        };
+    }
+
+    /**
+     * reset the TupleDesc of thi tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        disc = td;
+        id = null;
+        flds = new Field[td.numFields()];
+    }
+}
